ee90b1f28a344efa111a9ca771aaddf0a03e99ec
parthpateljnv <parthpateljnv@gmail.com>
2022-09-10 18:09:52 +0530

    new2

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -4,7 +4,7 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request. deployment slot')
+    logging.info('Python HTTP trigger function processed a request. deployment slot staging ')
 
     name = req.params.get('name')
     if not name:
